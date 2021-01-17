d46c88f47af6e054491b55b2ec544284598e9044
B.simak <b.simak@gmail.com>
2021-01-17 01:02:43 +0100

    Initial commit

---
+++ SpringInvoice/src/test/java/com/example/SpringInvoice/SpringInvoiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringInvoice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringInvoiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
